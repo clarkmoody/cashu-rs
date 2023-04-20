b025b6b95e8fc7cf0229e0cb8088d36b92b1ec35
Clark Moody <clark@clarkmoody.com>
2023-04-20 15:47:49 -0500

    Keyset module with ID generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
-pub fn hello() -> bool {
-    true
-}
+use serde::{Deserialize, Serialize};
+
+pub mod keyset;
+
+/// Number of satoshis
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct Amount(#[serde(with = "bitcoin::amount::serde::as_sat")] bitcoin::Amount);
