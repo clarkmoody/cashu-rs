51014f787fd5af920dc0909d4e4970d5acd7e855
Clark Moody <clark@clarkmoody.com>
2023-04-21 00:15:41 -0500

    Basic ecash types with token encoding tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-use serde::{Deserialize, Serialize};
-
+pub mod ecash;
 pub mod keyset;
 
-/// Number of satoshis
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
-#[serde(transparent)]
-pub struct Amount(#[serde(with = "bitcoin::amount::serde::as_sat")] bitcoin::Amount);
+pub use ecash::Amount;
